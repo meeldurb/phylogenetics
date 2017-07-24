731b38fd430dc72b4b5fb92257a5680f3864ccc5
Melanie vd Bosch <meeldurb@gmail.com>
2017-07-24 14:35:07 +0200

    changes are redundant, because i use simens script

--- auto.root_salmonid_clans_MvdB.R
+++ auto.root_salmonid_clans_MvdB.R
@@ -8,37 +8,37 @@
 
 #####___________ install & load packages and functions ____________####
 
-install.packages("ape", repos = "http://cran.rstudio.com/")
-install.packages("phangorn", repos = "http://cran.rstudio.com/")
+#install.packages("ape", repos = "http://cran.rstudio.com/")
+#install.packages("phangorn", repos = "http://cran.rstudio.com/")
 
-library("ape")
-library("phangorn")
+#library("ape")
+#library("phangorn")
 
 
 ######_________________ load example data _________________#####
 # load tree
-tr.n = "((((((((Omyk2|CIGENEomyV6.41072:0.00055,Omyk|GSONMT00002897001:0.03238)0.966:0.00292,Ssal|XP_013993288.1:0.00055):0.00159,Okis|gb|GDQG01036265.1||m.63677:0.00934)0.823:0.00152,Tthy|Tthy_00032335-RD:0.00413)0.505:0.00907,(((Omyk2|CIGENEomyV6.34600:0.00054,Omyk|GSONMT00074661001:0.00055)0.984:0.0156,Ssal|XP_014052884.1:0.0029)0.966:0.00343,Tthy|Tthy_00027454-RA:0.00951)0.969:0.00438)0.999:0.00155,Eluc|XP_010902685.1:0.01043)0.816:0.0661,Olat|ENSORLP00000000831.1:0.02215)1.000:0,Gacu|ENSGACP00000026666.1:0.01773)Root;"
-tree = read.tree(text=tr.n)
-plot(tree)
-
-# load tree with mammals
-tr.m = "((((((((Omyk2|CIGENEomyV6.41072:0.00055,Omyk|GSONMT00002897001:0.03238)0.966:0.00292,Ssal|XP_013993288.1:0.00055):0.00159,Okis|gb|GDQG01036265.1||m.63677:0.00934)0.823:0.00152,Tthy|Tthy_00032335-RD:0.00413)0.505:0.00907,(((Omyk2|CIGENEomyV6.34600:0.00054,Omyk|GSONMT00074661001:0.00055)0.984:0.0156,Ssal|XP_014052884.1:0.0029)0.966:0.00343,Tthy|Tthy_00027454-RA:0.00951)0.969:0.00438)0.999:0.00155,Eluc|XP_010902685.1:0.01043)0.816:0.0661,Mmus|ENSORLP00000000831.1:0.02215)1.000:0,Hsap|ENSGACP00000026666.1:0.01773)Root;"
-tree = read.tree(text=tr.m)
-plot(tree)
-
-
-# tree = rtree(10)
-# plot(tree, show.tip.label = FALSE)
-nodelabels()
-tiplabels()
-Ancestors(tree, 1:3, "all")
-Children(tree, 23)
-Descendants(tree, 13, "tips")
-Siblings(tree, 21)
+# tr.n = "((((((((Omyk2|CIGENEomyV6.41072:0.00055,Omyk|GSONMT00002897001:0.03238)0.966:0.00292,Ssal|XP_013993288.1:0.00055):0.00159,Okis|gb|GDQG01036265.1||m.63677:0.00934)0.823:0.00152,Tthy|Tthy_00032335-RD:0.00413)0.505:0.00907,(((Omyk2|CIGENEomyV6.34600:0.00054,Omyk|GSONMT00074661001:0.00055)0.984:0.0156,Ssal|XP_014052884.1:0.0029)0.966:0.00343,Tthy|Tthy_00027454-RA:0.00951)0.969:0.00438)0.999:0.00155,Eluc|XP_010902685.1:0.01043)0.816:0.0661,Olat|ENSORLP00000000831.1:0.02215)1.000:0,Gacu|ENSGACP00000026666.1:0.01773)Root;"
+# tree = read.tree(text=tr.n)
+# plot(tree)
 # 
-
-
-input.tree = tree
+# # load tree with mammals
+# tr.m = "((((((((Omyk2|CIGENEomyV6.41072:0.00055,Omyk|GSONMT00002897001:0.03238)0.966:0.00292,Ssal|XP_013993288.1:0.00055):0.00159,Okis|gb|GDQG01036265.1||m.63677:0.00934)0.823:0.00152,Tthy|Tthy_00032335-RD:0.00413)0.505:0.00907,(((Omyk2|CIGENEomyV6.34600:0.00054,Omyk|GSONMT00074661001:0.00055)0.984:0.0156,Ssal|XP_014052884.1:0.0029)0.966:0.00343,Tthy|Tthy_00027454-RA:0.00951)0.969:0.00438)0.999:0.00155,Eluc|XP_010902685.1:0.01043)0.816:0.0661,Mmus|ENSORLP00000000831.1:0.02215)1.000:0,Hsap|ENSGACP00000026666.1:0.01773)Root;"
+# tree = read.tree(text=tr.m)
+# plot(tree)
+# 
+# 
+# # tree = rtree(10)
+# # plot(tree, show.tip.label = FALSE)
+# nodelabels()
+# tiplabels()
+# Ancestors(tree, 1:3, "all")
+# Children(tree, 23)
+# Descendants(tree, 13, "tips")
+# Siblings(tree, 21)
+# # 
+# 
+# 
+# input.tree = tree
 
 
 ######_________________ function to put the root on the trees _________________#####
